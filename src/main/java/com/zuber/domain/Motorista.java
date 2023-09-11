1ed0e6e8b9a6aa869fd49b8bf0f5fefbc28635b7
Bruna Wesselka <bruna.wesselka@hotmail.com>
2023-09-11 14:01:09 -0300

    "Adicionando conexão do banco de dados"

--- src/main/java/com/zuber/domain/Motorista.java
+++ src/main/java/com/zuber/domain/Motorista.java
@@ -1,8 +1,25 @@
 package com.zuber.domain;
-import lombok.
+
 public class Motorista {
     private String nomeMotorista;
     private String veiculo;
-    private Long cnh;
+    private int cnh;
 
+    public Motorista(String nomeMotorista, String veiculo, int cnh) {
+        this.nomeMotorista = nomeMotorista;
+        this.veiculo = veiculo;
+        this.cnh = cnh;
+    }
+
+    public String getNomeMotorista() {
+        return nomeMotorista;
+    }
+
+    public String getVeiculo() {
+        return veiculo;
+    }
+
+    public int getCnh() {
+        return cnh;
+    }
 }
--- src/main/java/com/zuber/domain/MotoristaService.java
+++ src/main/java/com/zuber/domain/MotoristaService.java
@@ -3,12 +3,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MotoristaService {
-    public List<Motorista> get Motoristas(){
+    public List<Motorista> getMotoristas() {
         List<Motorista> motoristas = new ArrayList<>();
 
-        motoristas.add(new Motorista(1L, "Jabel"));
-        motoristas.add(new Motorista(1L, "Caique"));
-        motoristas.add(new Motorista(1L, "Robson"));
+        motoristas.add(new Motorista("Jabel", "Onix", 45451));
+        motoristas.add(new Motorista("Caique", "Sandero", 569465));
+        motoristas.add(new Motorista("Robson", "Corolla", 55645631));
 
         return motoristas;
     }
